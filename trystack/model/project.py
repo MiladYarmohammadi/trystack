6ee7477d55ea26ca190d4d58d21264a1ab5e0140
milad <milad.sphinx@gmail.com>
2022-08-27 12:16:47 +0400

    Add Database Structure and Project Database Model

---
+++ trystack/model/project.py
@@ -0,0 +1,11 @@
+from trystack.config import Config
+from trystack.trystack import db
+from trystack.util import uuidgen, datetime_now
+
+
+class Project(db.Model):
+    id = db.Column(db.String(64), primary_key=True, default=uuidgen)
+    name = db.Column(db.String(256), unique=True, index=True, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime_now)
+    updated_at = db.Column(db.DateTime, nullable=True)
+    status = db.Column(db.Integer, nullable=False, default=Config.DEFAULT_PROJECT_STATUS)
