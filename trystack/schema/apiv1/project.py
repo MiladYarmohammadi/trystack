9cfbee8933291a278887580dd5f1ea1bc4a03ecd
milad <milad.sphinx@gmail.com>
2022-09-08 18:39:17 +0400

    add marshmallow and project model schema

---
+++ trystack/schema/apiv1/project.py
@@ -0,0 +1,13 @@
+from trystack.trystack import ma
+from trystack.model import Project
+
+
+class ProjectSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Project
+
+    id = ma.auto_field(dump_only=True)
+    name = ma.auto_field()
+    created_at = ma.auto_field(dump_only=True)
+    updated_at = ma.auto_field(dump_only=True)
+    status = ma.auto_field()
--- trystack/trystack.py
+++ trystack/trystack.py
@@ -1,5 +1,6 @@
 from json import load as json_load
 from flask import Blueprint, Flask
+from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
@@ -8,6 +9,8 @@ from trystack.config import Config
 
 db = SQLAlchemy()
 mg = Migrate()
+ma = Marshmallow()
+
 apiv1_bp = Blueprint(name="apiv1_bp", import_name=__name__, url_prefix="/api/v1")
 apiv1 = Api(apiv1_bp)
 
@@ -21,5 +24,6 @@ def create_app(config_file=None):
         app.config.from_file(config_file, load=json_load)
     db.init_app(app)
     mg.init_app(app, db)
+    ma.init_app(app)
     app.register_blueprint(apiv1_bp)  # Register /api/v1 blueprint to main app
     return app
